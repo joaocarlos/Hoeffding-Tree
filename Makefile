50f3a4177585793a45fa0b0b717f0c788e05225d
João Carlos <joaocarlos@ufrb.edu.br>
2023-05-16 11:36:27 +0100

    Introduced optimisation for functions: - Min-max normalisation - Min-man - Evaluate Split - Update - Update Quantile I also made some changes on Makefile - Introduced optimisation flags - `make clean` now also removes gprof files (be careful!)

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ CXX = g++
 
 BUILD_DIR ?= .build
 SRC_DIRS ?= src tests
+GPROF_DIR ?= gprof
 TARGET_EXEC ?= $(BUILD_DIR)/$(TARGET_EXEC_NAME)
 
 SRCS := $(shell find $(SRC_DIRS) -maxdepth 1 -name "*.cpp" -or -name "*.c" ! -name "test.c" -or -name "*.s")
@@ -14,18 +15,33 @@ DEPS := $(OBJS:.o=.d)
 
 INC_DIRS := $(shell find $(SRC_DIRS) -type d)
 INC_FLAGS := $(addprefix -I,$(INC_DIRS))
-# PROF_FLAGS := -pg 
+# Gprof flags
+# -fprile-arcs: generate extra code to write profile information suitable for the analysis program gprof
+# -ftest-coverage: compile with coverage information for each object file
+# -fno-inline: do not inline functions
+# -fno-inline-small-functions: do not inline functions that are both small and marked inline
+# -fno-default-inline: do not make any functions inline by default
+PROF_FLAGS := -pg -fprofile-arcs -ftest-coverage -fno-inline -fno-inline-small-functions -fno-default-inline 
 OPT_FLAGS := -O3
 EXTRA_FLAGS := -std=c++17 # only on Mac runing clang++ (-g)
-SCENARIO_FLAG := -DSCENARIO=1
+SCENARIO := 1
+SCENARIO_FLAG := -DSCENARIO=$(SCENARIO)
+MINMAX_NORMALIZE_OPT := 0
+MINMAX_OPT := 0
+UPDATE_OPT := 0
+EVALUALTE_SPLIT_OPT := 0
+UPDATE_QUANTILES_OPT := 0
+OPTMIZATION_FLAGS := -D_MINMAX_NORMALIZE_OPT_=$(MINMAX_NORMALIZE_OPT) -D_MINMAX_OPT_=$(MINMAX_OPT) -D_UPDATE_OPT_=$(UPDATE_OPT) -D_EVALUALTE_SPLIT_OPT_=$(EVALUALTE_SPLIT_OPT) -D_UPDATE_QUANTILES_OPT_=$(UPDATE_QUANTILES_OPT)
 
-CPPFLAGS ?= $(INC_FLAGS) -MMD -MP $(PROF_FLAGS) $(EXTRA_FLAGS) $(OPT_FLAGS) $(SCENARIO_FLAG) -fPIE -Wno-unused-label 
+CPPFLAGS ?= $(INC_FLAGS) -MMD -MP $(PROF_FLAGS) $(EXTRA_FLAGS) $(OPT_FLAGS) $(SCENARIO_FLAG) $(OPTMIZATION_FLAGS) -fPIE -Wno-unused-label 
 
 # -I/tools/Xilinx/Vitis_HLS/2020.2/include 
 
 LDFLAGS := 
 
-$(TARGET_EXEC): $(OBJS)
+all: clean $(TARGET_EXEC)_$(SCENARIO) run gprof graph
+
+$(TARGET_EXEC)_$(SCENARIO): $(OBJS)
 	$(CC) $(OBJS) $(PROF_FLAGS) -o $@ $(LDFLAGS)
 
 # assembly
@@ -43,23 +59,22 @@ $(BUILD_DIR)/%.cpp.o: %.cpp
 	$(MKDIR_P) $(dir $@)
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $< -o $@
 
-
 .PHONY: clean
 .PHONY: run
 .PHONY: gprof
 .PHONY: graph
 
 clean:
-	$(RM) -r $(BUILD_DIR)
+	$(RM) -r $(BUILD_DIR) $(GPROF_DIR)/*
 
 run:
-	./$(TARGET_EXEC)
+	./$(TARGET_EXEC)_$(SCENARIO)
 
-gprof:
-	gprof $(TARGET_EXEC) gmon.out > gprof.txt | gprof2dot gprof.txt > gprof.dot
+gprof: run
+	gprof $(TARGET_EXEC)_$(SCENARIO) gmon.out > $(GPROF_DIR)/gprof_$(SCENARIO).txt | gprof2dot $(GPROF_DIR)/gprof_$(SCENARIO).txt > $(GPROF_DIR)/gprof_$(SCENARIO).dot
 
-graph:
-	dot -Tpng gprof.dot -o gprof.png
+graph: 
+	dot -Tpng $(GPROF_DIR)/gprof_$(SCENARIO).dot -o $(GPROF_DIR)/gprof_$(SCENARIO).png
 
 -include $(DEPS)
 
--- tests/main.cpp
+++ tests/main.cpp
@@ -23,11 +23,13 @@
 #include "../src/JsonExporter.hpp"
 #include "../src/Node.hpp"
 #include "../src/TypeChooser.hpp"
+// #include "DataHandler.hpp"
+#include "Optimisation.hpp"
 #include "Tester.hpp"
 
+// #define _PLAIN_DATASET_
 #define _INPUT_FILE_TRAIN_DATASET_
 #define _INPUT_FILE_TEST_DATASET_
-// #define _PLAIN_DATASET_
 
 /*
  * Scenario 1: Offline Training and Testing without online training
@@ -37,11 +39,11 @@
  *   #define _OFFLINE_TRAINING_
  *   #define _ONLINE_TRAINING_AND_TESTING_
  * Scenario 3: Online Training and Testing
- *   #define _ONLINE_TRAINING_AND_TESTING_
+ *   #define _ALL_ONLINE_TRAINING_AND_TESTING_
  */
 
 #ifndef SCENARIO
-#define SCENARIO 3
+#define SCENARIO 2
 #endif
 
 /*
@@ -57,22 +59,18 @@
 #define _ALL_ONLINE_TRAINING_AND_TESTING_
 #endif
 
+#define NUM_TRAINING_SAMPLES 4336
+#define NUM_TESTING_SAMPLES 1082
+#define NUM_FEATURES 43
+#define NUM_CLASSES 6
+
 #ifndef _GENERATE_TREE_JSON_
 // #define _GENERATE_TREE_JSON_
 #endif
 
 #ifndef _ACCURACY_CHECK_
-#define _ACCURACY_CHECK_
+// #define _ACCURACY_CHECK_
 #endif
-
-#ifndef _MIN_MAX_SPLIT_OPT_
-// #define _MIN_MAX_SPLIT_OPT_
-#endif
-
-#define NUM_TRAINING_SAMPLES 4336
-#define NUM_TESTING_SAMPLES 1082
-#define NUM_FEATURES 43
-#define NUM_CLASSES 6
 
 typedef char CLASS_ID_TYPE;
 typedef float DATA_TYPE;
@@ -162,6 +160,7 @@ FixedTree::data_t scale(FixedTree::data_t a) { return a * 8; }
  * set of points.
  * minmax(min, max, NUM_TRAINING_SAMPLES, irisDataset, NUM_FEATURES)
  */
+#if _MINMAX_OPT_ == 0
 void minmax(DATA_TYPE *min, DATA_TYPE *max, int num_points,
             DATA_TYPE known_points[][NUM_FEATURES + 1], int num_features) {
 
@@ -171,7 +170,6 @@ void minmax(DATA_TYPE *min, DATA_TYPE *max, int num_points,
         // printf("%e, %e\n", MIN_FP_VAL, MAX_FP_VAL);
     }
 
-#ifndef _MIN_MAX_SPLIT_OPT_
     for (int i = 0; i < num_points; i++) {
         for (int j = 0; j < num_features; j++) {
             if (known_points[i][j] < min[j])
@@ -180,30 +178,169 @@ void minmax(DATA_TYPE *min, DATA_TYPE *max, int num_points,
                 max[j] = known_points[i][j];
         }
     }
-#else
-    // Split Min and Max into two loops to promote vectorization and reduce
-    // misses in the cache.
-    for (int i = 0; i < num_points; i++) {
-        for (int j = 0; j < num_features; j++) {
-            if (known_points[i][j] < min[j])
-                min[j] = known_points[i][j];
-        }
-    }
-    for (int i = 0; i < num_points; i++) {
-        for (int j = 0; j < num_features; j++) {
-            if (known_points[i][j] > max[j])
-                max[j] = known_points[i][j];
-        }
-    }
-#endif
 }
 
+/*
+ * Determine the min and max values for each feature for a
+ * set of points.
+ * minmax(min, max, NUM_TRAINING_SAMPLES, irisDataset, NUM_FEATURES)
+ *
+ * Loop unrolling and array blocking
+ */
+#elif _MINMAX_OPT_ == 1
+void minmax(DATA_TYPE *min, DATA_TYPE *max, int num_points,
+            DATA_TYPE known_points[][NUM_FEATURES + 1], int num_features) {
+
+    // Loop unrolling factor
+    const int UNROLL_FACTOR = 4;
+
+    // Array blocking factors
+    const int BLOCK_SIZE = 64;
+    const int BLOCK_FACTOR = 4;
+
+    // Compute the number of blocks
+    const int num_blocks = (num_points + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    // Compute the block size for the unrolled loop
+    const int unrolled_block_size = BLOCK_SIZE / UNROLL_FACTOR;
+
+    for (int j = 0; j < num_features; j++) {
+        min[j] = MAX_FP_VAL;
+        max[j] = MIN_FP_VAL;
+    }
+
+    // Loop over blocks
+    for (int b = 0; b < num_blocks; b++) {
+        // Compute the block indices
+        const int start_index = b * BLOCK_SIZE;
+        const int end_index = MIN((b + 1) * BLOCK_SIZE, num_points);
+
+        // Loop over features
+        for (int j = 0; j < num_features; j++) {
+            // Loop over unrolled blocks
+            for (int k = 0; k < unrolled_block_size; k++) {
+                // Compute the row index
+                const int i = start_index + k * UNROLL_FACTOR;
+
+                // Unrolled loop for min
+                for (int u = 0; u < UNROLL_FACTOR; u++) {
+                    const DATA_TYPE val = known_points[i + u][j];
+                    if (val < min[j])
+                        min[j] = val;
+                }
+            }
+
+            // Loop over unrolled blocks
+            for (int k = 0; k < unrolled_block_size; k++) {
+                // Compute the row index
+                const int i = start_index + k * UNROLL_FACTOR;
+
+                // Unrolled loop for max
+                for (int u = 0; u < UNROLL_FACTOR; u++) {
+                    const DATA_TYPE val = known_points[i + u][j];
+                    if (val > max[j])
+                        max[j] = val;
+                }
+            }
+        }
+    }
+}
+
+/*
+ * Determine the min and max values for each feature for a
+ * set of points.
+ * minmax(min, max, NUM_TRAINING_SAMPLES, irisDataset, NUM_FEATURES)
+ *
+ * Loop fusion
+ */
+#elif _MINMAX_OPT_ == 2
+void minmax(DATA_TYPE *min, DATA_TYPE *max, int num_points,
+            DATA_TYPE known_points[][NUM_FEATURES + 1], int num_features) {
+
+    for (int j = 0; j < num_features; j++) {
+        min[j] = MAX_FP_VAL;
+        max[j] = MIN_FP_VAL; // MAX in float.h
+    }
+
+    for (int i = 0; i < num_points; i++) {
+        for (int j = 0; j < num_features; j++) {
+            if (known_points[i][j] < min[j])
+                min[j] = known_points[i][j];
+            if (known_points[i][j] > max[j])
+                max[j] = known_points[i][j];
+        }
+    }
+}
+
+/*
+ * Determine the min and max values for each feature for a
+ * set of points.
+ * minmax(min, max, NUM_TRAINING_SAMPLES, irisDataset, NUM_FEATURES)
+ *
+ * Loop tiling
+ */
+#elif _MINMAX_OPT_ == 3
+void minmax(DATA_TYPE *min, DATA_TYPE *max, int num_points,
+            DATA_TYPE known_points[][NUM_FEATURES + 1], int num_features) {
+
+    int block_size = 16;
+
+    for (int j = 0; j < num_features; j++) {
+        min[j] = MAX_FP_VAL;
+        max[j] = MIN_FP_VAL;
+    }
+
+    for (int i = 0; i < num_points; i += block_size) {
+        for (int j = 0; j < num_features; j++) {
+            DATA_TYPE block_min = MAX_FP_VAL;
+            DATA_TYPE block_max = MIN_FP_VAL;
+            for (int k = i; k < i + block_size && k < num_points; k++) {
+                if (known_points[k][j] < block_min)
+                    block_min = known_points[k][j];
+                if (known_points[k][j] > block_max)
+                    block_max = known_points[k][j];
+            }
+            if (block_min < min[j])
+                min[j] = block_min;
+            if (block_max > max[j])
+                max[j] = block_max;
+        }
+    }
+}
+/*
+ * Determine the min and max values for each feature for a
+ * set of points.
+ * minmax(min, max, NUM_TRAINING_SAMPLES, irisDataset, NUM_FEATURES)
+ *
+ * Loop interchange
+ */
+#elif _MINMAX_OPT_ == 4
+void minmax(DATA_TYPE *min, DATA_TYPE *max, int num_points,
+            DATA_TYPE known_points[][NUM_FEATURES + 1], int num_features) {
+
+    for (int j = 0; j < num_features; j++) {
+        min[j] = MAX_FP_VAL;
+        max[j] = MIN_FP_VAL; // MAX in float.h
+    }
+
+    for (int j = 0; j < num_features; j++) {
+        for (int i = 0; i < num_points; i++) {
+            if (known_points[i][j] < min[j])
+                min[j] = known_points[i][j];
+            if (known_points[i][j] > max[j])
+                max[j] = known_points[i][j];
+        }
+    }
+}
+#endif
 /*
  * Normalize the features of each point using minmax normalization.
  * minmax_normalize(min, max, NUM_TRAINING_SAMPLES, irisDataSet, NUM_FEATURES)
  */
+#if _MINMAX_NORMALIZE_OPT_ == 0
 void minmax_normalize(DATA_TYPE *min, DATA_TYPE *max, int num_points,
                       DATA_TYPE points[][NUM_FEATURES + 1], int num_features) {
+
     for (int i = 0; i < num_points; i++) {
         for (int j = 0; j < num_features; j++) {
             DATA_TYPE nfeature =
@@ -220,6 +357,92 @@ void minmax_normalize(DATA_TYPE *min, DATA_TYPE *max, int num_points,
         // show_point(points[i], num_features); 
     }
 }
+
+/*
+ * Normalize the features of each point using minmax normalization.
+ * minmax_normalize(min, max, NUM_TRAINING_SAMPLES, irisDataSet, NUM_FEATURES)
+ *
+ * Unroll the inner loop to promote vectorization.
+ */
+
+#elif _MINMAX_NORMALIZE_OPT_ == 1
+void minmax_normalize(DATA_TYPE *min, DATA_TYPE *max, int num_points,
+                      DATA_TYPE points[][NUM_FEATURES + 1], int num_features) {
+    for (int i = 0; i < num_points; i++) {
+        for (int j = 0; j < num_features; j++) {
+            DATA_TYPE nfeature[4];
+            /* I am assuming that the inner loop will be unrolled at compilation
+             * time  */
+            for (int k = 0; k < 4; k++) {
+                nfeature[k] = (DATA_TYPE)((points[i][j + k] - min[j + k]) /
+                                          (max[j + k] - min[j + k]));
+
+                // in case the normalization returns a NaN or INF
+                if (isnan(nfeature[k]))
+                    nfeature[k] = (DATA_TYPE)0.0;
+                else if (isinf(nfeature[k]))
+                    nfeature[k] = (DATA_TYPE)1.0;
+            }
+
+            points[i][j] = nfeature[0];
+            points[i][j + 1] = nfeature[1];
+            points[i][j + 2] = nfeature[2];
+            points[i][j + 3] = nfeature[3];
+        }
+    }
+}
+
+/*
+ * Normalize the features of each point using minmax normalization.
+ * minmax_normalize(min, max, NUM_TRAINING_SAMPLES, irisDataSet, NUM_FEATURES)
+ *
+ * Fuse the inner loop to promote vectorization.
+ */
+#elif _MINMAX_NORMALIZE_OPT_ == 2
+void minmax_normalize(DATA_TYPE *min, DATA_TYPE *max, int num_points,
+                      DATA_TYPE points[][NUM_FEATURES + 1], int num_features) {
+    for (int i = 0; i < num_points * num_features; i++) {
+        int p = i / num_features;
+        int f = i % num_features;
+
+        DATA_TYPE nfeature =
+            (DATA_TYPE)((points[p][f] - min[f]) / (max[f] - min[f]));
+
+        // in case the normalization returns a NaN or INF
+        if (isnan(nfeature))
+            nfeature = (DATA_TYPE)0.0;
+        else if (isinf(nfeature))
+            nfeature = (DATA_TYPE)1.0;
+
+        points[p][f] = nfeature;
+    }
+}
+/*
+ * Normalize the features of each point using minmax normalization.
+ * minmax_normalize(min, max, NUM_TRAINING_SAMPLES, irisDataSet, NUM_FEATURES)
+ *
+ * Loop interchange to to improve memory access patterns and cache utilization
+ */
+#elif _MINMAX_NORMALIZE_OPT_ == 3
+void minmax_normalize(DATA_TYPE *min, DATA_TYPE *max, int num_points,
+                      DATA_TYPE points[][NUM_FEATURES + 1], int num_features) {
+
+    for (int j = 0; j < num_features; j++) {
+        DATA_TYPE range = max[j] - min[j];
+        for (int i = 0; i < num_points; i++) {
+            DATA_TYPE nfeature = (DATA_TYPE)((points[i][j] - min[j]) / range);
+
+            // in case the normalization returns a NaN or INF
+            if (isnan(nfeature))
+                nfeature = (DATA_TYPE)0.0;
+            else if (isinf(nfeature))
+                nfeature = (DATA_TYPE)1.0;
+
+            points[i][j] = nfeature;
+        }
+    }
+}
+#endif
 
 /*
  * Normalize the features of each point using minmax normalization.
@@ -252,7 +475,6 @@ int main() {
     minmax_normalize(min, max, NUM_TRAINING_SAMPLES, trainDataset,
                      NUM_FEATURES);
 #endif
-
     // Run tests
     Tester ts;
 
@@ -308,7 +530,6 @@ int main() {
         file << result;
         file.close();
 #endif
-
         return std::make_pair(true, "Will always return true");
     });
 #endif
@@ -351,8 +572,6 @@ int main() {
 
         // tree.getRootNode()->getData().evaluateSplit();
 
-        return std::make_pair(true, "Will always return true");
-
 #ifdef _GENERATE_TREE_JSON_
         Tree treeCopy(tree.getR(), tree.getSigma(), tree.getTau());
 
@@ -368,6 +587,7 @@ int main() {
         file << result;
         file.close();
 #endif
+        return std::make_pair(true, "Will always return true");
     });
 #endif
 
@@ -428,8 +648,6 @@ int main() {
 
         // tree.getRootNode()->getData().evaluateSplit();
 
-        return std::make_pair(true, "Will always return true");
-
 #ifdef _GENERATE_TREE_JSON_
         Tree treeCopy(tree.getR(), tree.getSigma(), tree.getTau());
 
@@ -445,6 +663,7 @@ int main() {
         file << result;
         file.close();
 #endif
+        return std::make_pair(true, "Will always return true");
     });
 #endif
 
